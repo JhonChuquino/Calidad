aafe45c6f46aaf297ba9399a450d26a84793a4d8
JhonChuquino <126582208+JhonChuquino@users.noreply.github.com>
2025-09-23 15:11:39 -0500

    Delete P1 directory

--- P1/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "productos_url" {
-  value = "${aws_apigatewayv2_api.productos_api.api_endpoint}/products"
-}
-
-output "resenas_url" {
-  value = "${aws_apigatewayv2_api.resenas_api.api_endpoint}/reviews"
-}
--- P1/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "Región AWS para desplegar"
-  type        = string
-  default     = "us-east-1"
-}
