72b1976c122cda72a7c3af9a6833c82ead366714
JhonChuquino <126582208+JhonChuquino@users.noreply.github.com>
2025-10-09 19:42:36 -0500

    Update main.tf

--- farmacia_api/terraform/main.tf
+++ farmacia_api/terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "farmacia_api" {
   instance_type               = var.instance_type
   key_name                    = var.key_name
   subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.farmacia.id]
+  vpc_security_group_ids      = [aws_security_group.farmacia_sg.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
